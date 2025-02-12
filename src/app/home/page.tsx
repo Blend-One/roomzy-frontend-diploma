c243e761bc93e1bc083cad5778b13650689b9ac3
Daniel Kaziev <kaziev30@mail.ru>
2025-02-13 00:11:43 +0500

    init next app

---
+++ src/app/home/page.tsx
@@ -0,0 +1,4 @@
+const Home = async () => {
+    return <>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio impedit voluptatibus, harum at non exercitationem debitis alias mollitia quas nemo dolores quasi fugit similique eius repellat? Aliquam sunt voluptates ullam.</>
+};
+export default Home;
