ab9e75a99f32e5fa63247f4e25687ffdf4d823d4
Daniel Kaziev <kaziev30@mail.ru>
2025-03-09 21:57:57 +0500

    feat: add rental support to the publishing component and update localization

--- src/pages/Publication/Publication.tsx
+++ src/pages/Publication/Publication.tsx
@@ -16,6 +16,8 @@ import {
 import ImgGallery from "../../components/ImgGallery";
 import { useMemo } from "react";
 import { ISpaceDetails } from "../../types/space";
+import { useTranslation } from "react-i18next";
+import { getRentTypeCompare } from "../../utils/compare";
 
 const PublicationTitle = styled(Paper)(({ theme }) => ({
   fontSize: "1.5rem",
@@ -34,11 +36,11 @@ const DetailsComponent = ({ data }: { data: ISpaceDetails }) => {
         <Paper elevation={5} sx={{ padding: 5 }}>
           <Stack spacing={2}>
             {data.data.map((dit, index) => (
-              <Stack key={index} spacing={2}>
+              <Stack key={index}>
                 <Stack>{dit.name}</Stack>
                 <Stack direction="row">
                   {dit.details.map((tye) => (
-                    <Chip key={tye.id} label={tye.name} />
+                    <Chip sx={{ mt: 1, mr: 1 }} key={tye.id} label={tye.name} />
                   ))}
                 </Stack>
               </Stack>
@@ -54,6 +56,7 @@ const Publication = () => {
   const { id } = useParams();
   const { data } = useGetSpaceByIdQuery(id ?? "");
   const { data: details } = useGetSpaceDetailsByIdQuery(data.id ?? "");
+  const { t } = useTranslation(["space", "components"]);
 
   const images = useMemo(
     () =>
@@ -75,19 +78,31 @@ const Publication = () => {
           <Grid size={{ sm: 4 }}>
             <Paper elevation={5} sx={{ padding: 4 }}>
               <Stack spacing={1}>
-                <Typography>Price: {data.price}</Typography>
-                <Typography>Payment Type: {data.paymentType}</Typography>
-                <Typography>Street: {data.street}</Typography>
-                <Typography>Building: {data.building}</Typography>
                 <Typography>
-                  Commercial: {data.isCommercial ? "Yes" : "No"}
+                  {t("I18N_SPACE_PRICE", {
+                    price: data.price,
+                    type: getRentTypeCompare(data.paymentType),
+                  })}
                 </Typography>
-                <Typography>Square: {data.square} m²</Typography>
-                <Typography>Floors: {data.floors}</Typography>
                 <Typography>
-                  Deposit: {data.hasDeposit ? "Yes" : "No"}
+                  {t("I18N_SPACE_ADDRESS")}: {data.street}, {data.building}
                 </Typography>
-                <Button variant="contained">Арендовать</Button>
+                <Typography>
+                  {data.isCommercial
+                    ? t("I18N_SPACE_COMMERCE")
+                    : t("I18N_SPACE_LIVING")}
+                </Typography>
+                <Typography>
+                  {t("I18N_SPACE_SQUARE", { square: data.square })}
+                </Typography>
+                <Typography>
+                  {t("I18N_SPACE_FLOORS", { floors: data.floors })}
+                </Typography>
+                <Typography>
+                  {t("I18N_SPACE_NEED_DEPOSIT")}:{" "}
+                  {data.hasDeposit ? "Yes" : "No"}
+                </Typography>
+                <Button variant="contained">{t("I18N_SPACE_RENT")}</Button>
               </Stack>
             </Paper>
           </Grid>
