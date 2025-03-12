94435f0a2f0b1758c6727975ba7e3090a75126aa
Daniel Kaziev <kaziev30@mail.ru>
2025-03-13 01:03:02 +0500

    draft: add BasicTable component and enhance MyPublications and MyRentals pages with search and create functionality

--- src/pages/Account/MyPublications/MyPublications.tsx
+++ src/pages/Account/MyPublications/MyPublications.tsx
@@ -1,12 +1,22 @@
-import { Typography } from "@mui/material";
 import Page from "../../../components/Page";
 import AccountWrapperWidget from "../../../widgets/AccountWrapperWidget";
+import BasicTable from "../../../components/Table";
+import { Button, Stack, TextField } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+import SearchIcon from '@mui/icons-material/Search';
 
 const MyPublications = () => {
   return (
-    <Page withPadding>
+    <Page withPadding >
       <AccountWrapperWidget>
-        <Typography sx={{ background: "rgb(255,0,0, 0.5)" }}>data</Typography>
+        <Stack spacing={2} flexGrow={1}>
+          <Stack direction="row" spacing={2}>
+          <TextField size="small" sx={{flexGrow: 1}} label="Search" />
+            <Button startIcon={<SearchIcon/>} variant="contained">Search</Button>
+            <Button startIcon={<AddIcon/>} variant="contained">Create Publication</Button>
+          </Stack>
+        <BasicTable/>
+        </Stack>
       </AccountWrapperWidget>
     </Page>
   );
--- src/pages/Account/MyRentals/MyRentals.tsx
+++ src/pages/Account/MyRentals/MyRentals.tsx
@@ -1,13 +1,10 @@
-import { Typography } from "@mui/material";
 import Page from "../../../components/Page";
 import AccountWrapperWidget from "../../../widgets/AccountWrapperWidget";
 
 const MyRentals = () => {
   return (
     <Page withPadding>
-      <AccountWrapperWidget>
-        <Typography sx={{ background: "rgb(255,0,0, 0.5)" }}>data</Typography>
-      </AccountWrapperWidget>
+      <AccountWrapperWidget/>
     </Page>
   );
 };
