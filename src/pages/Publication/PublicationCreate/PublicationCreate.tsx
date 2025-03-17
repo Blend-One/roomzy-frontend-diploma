14d602e90f089fd16de178770b9c3747b5673019
Daniel Kaziev <kaziev30@mail.ru>
2025-03-17 23:52:17 +0500

    feat: implement form for creating publications with dynamic fields and validation

--- src/pages/Publication/PublicationCreate/PublicationCreate.tsx
+++ src/pages/Publication/PublicationCreate/PublicationCreate.tsx
@@ -1,12 +1,66 @@
-import { Stack } from "@mui/material";
+import { Grid2 as Grid } from "@mui/material";
 import Page from "../../../components/Page";
+import { FormProvider, useForm } from "react-hook-form";
+import { ISpace } from "../../../types/space";
+import TextFieldCustom from "../../../components/Forms/Inputs/TextFieldCustom";
+import NumberFieldCustom from "../../../components/Forms/Inputs/NumberFieldCustom";
 
 const PublicationCreate = () => {
+  const formMethods = useForm<ISpace>({
+    defaultValues: {
+      id: "",
+      imageUrl: "",
+      title: "",
+      price: 5000,
+      priceUnit: "PER_DAY",
+      street: "",
+      building: "1",
+      hasDeposit: true,
+      isCommercial: true,
+      square: 0,
+      floors: 0
+    }
+  });
+  
   return (
     <Page withPadding>
-      <Stack spacing={3} mt={4}>
-        Lorem ipsum dolor sit amet.
-      </Stack>
+      <FormProvider {...formMethods}>
+        <Grid mt={3} container spacing={3}>
+          <Grid size={12}>
+            <TextFieldCustom name={"id"} label={"id"}/>
+          </Grid>
+          <Grid size={12}>
+            <TextFieldCustom name={"imageUrl"} label={"imageUrl"}/>
+          </Grid>
+          <Grid size={12}>
+            <TextFieldCustom name={"title"} label={"title"}/>
+          </Grid>
+          <Grid size={12}>
+            <NumberFieldCustom name={"price"} label={"price"}/>
+          </Grid>
+          <Grid size={12}>
+            <TextFieldCustom name={"priceUnit"} label={"priceUnit"}/>
+          </Grid>
+          <Grid size={12}>
+            <TextFieldCustom name={"street"} label={"street"}/>
+          </Grid>
+          <Grid size={12}>
+            <TextFieldCustom name={"building"} label={"building"}/>
+          </Grid>
+          <Grid size={12}>
+            <TextFieldCustom name={"hasDeposit"} label={"hasDeposit"}/>
+          </Grid>
+          <Grid size={12}>
+            <TextFieldCustom name={"isCommercial"} label={"isCommercial"}/>
+          </Grid>
+          <Grid size={12}>
+            <TextFieldCustom name={"square"} label={"square"}/>
+          </Grid>
+          <Grid size={12}>
+            <TextFieldCustom name={"floors"} label={"floors"}/>
+          </Grid>
+        </Grid>
+      </FormProvider>
     </Page>
   );
 };
